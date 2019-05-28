567fee2991599ecfe971e3cfe6b2fd5126fdddb9
Alexander Senn <alexjsenn@gmail.com>
2019-05-27 21:40:15 -0300

    added more functionality in the new task page, but cant yet save the new task to the list object in the other stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,45 +15,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class taskListState extends State<TaskList>{
+class taskListState extends State<TaskList> {
   var taskList = <TaskInfo>[];
   final _fontSize = const TextStyle(fontSize: 18.0);
+  var taskListKey = GlobalKey;
 
-  Widget _buildList(){
+  Widget _buildList() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           final index = i ~/ 2;
-          if (index >= taskList.length) return null;
-          else if (i.isOdd) return Divider();
-          else return _buildRow(taskList[index]);
-        }
-      );
+          if (index >= taskList.length)
+            return null;
+          else if (i.isOdd)
+            return Divider();
+          else
+            return _buildRow(taskList[index]);
+        });
   }
 
-  Widget _buildRow (TaskInfo info){
+  Widget _buildRow(TaskInfo info) {
     final finished = info.finished;
     return ListTile(
-      title: Text(
-        info.getTitle(),
-        style: _fontSize,
-      ),
-      leading: Icon(
-        finished ? Icons.check_box : Icons.check_box_outline_blank,
-        color: finished ? Colors.green : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (finished){
-            info.finished = false;
-          } else {
-            info.finished = true;
-          }
+        title: Text(
+          info.getTitle(),
+          style: _fontSize,
+        ),
+        leading: Icon(
+          finished ? Icons.check_box : Icons.check_box_outline_blank,
+          color: finished ? Colors.green : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (finished) {
+              info.finished = false;
+            } else {
+              info.finished = true;
+            }
+          });
         });
-      }
-    );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -62,9 +64,9 @@ class taskListState extends State<TaskList>{
       ),
       body: _buildList(),
       floatingActionButton: FloatingActionButton(
-          onPressed: _createTask,
-          child: Icon(Icons.add),
-          backgroundColor: Colors.blue,
+        onPressed: _createTask,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
       ),
     );
   }
@@ -77,44 +79,216 @@ class taskListState extends State<TaskList>{
             appBar: AppBar(
               title: Text('New Task'),
             ),
-            body: TextFormField(
-              decoration: const InputDecoration(
-                contentPadding: const EdgeInsets.all(16.0),
-                hintText: 'Task',
-              ),
-              onFieldSubmitted: (String value) {
-                setState(() {
-                  taskList.add(TaskInfo(value, ''));
-                  Navigator.of(context).pop();
-                });
-              }
-            ),
+            body: taskForm()
+
+
+           /* TextFormField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(20.0),
+                  hintText: 'Task',
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                  ),
+                  border: UnderlineInputBorder(),
+                ),
+                style: TextStyle(
+                  height: 1.2,
+                ),
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                textCapitalization: TextCapitalization.sentences,
+                scrollPadding: EdgeInsets.all(16.0),
+                onFieldSubmitted: (String value) {
+                  setState(() {
+                    taskList.add(TaskInfo(value, ''));
+                    Navigator.of(context).pop();
+                  });
+                })*/
+
+            ,
           );
         },
       ),
     );
   }
-
-
 }
 
-class TaskList extends StatefulWidget{
+class _taskFormState extends State<taskForm> {
+  final formKey = GlobalKey<FormState>();
+
+  TaskInfo newTask;
+  String _taskTitle;
+  String _taskDescription;
+  DateTime _Date = DateTime.now();
+  TimeOfDay _Time = TimeOfDay.now();
+
+  saveNewTask() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: new ListView(
+        children: getFormWidget(),
+      ));
+  }
+
+  List<Widget> getFormWidget(){
+    List<Widget> formWidget = new List();
+
+    formWidget.add(new TextFormField(
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(20.0),
+          hintText: 'Task',
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 18,
+          ),
+          border: UnderlineInputBorder(),
+        ),
+        style: TextStyle(
+          height: 1.2,
+        ),
+        validator: (value) {
+          if (value.isEmpty) {return 'Enter task';}
+        },
+        maxLines: null,
+        keyboardType: TextInputType.multiline,
+        textCapitalization: TextCapitalization.sentences,
+        scrollPadding: EdgeInsets.all(16.0),
+        onSaved: (value) {
+          setState(() {
+            _taskTitle = value;
+          });
+        },
+        )
+    );
+
+    formWidget.add(new TextFormField(
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.all(20.0),
+        hintText: 'Description',
+        hintStyle: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 18,
+        ),
+        border: UnderlineInputBorder(),
+      ),
+      style: TextStyle(
+        height: 1.2,
+      ),
+      maxLines: null,
+      keyboardType: TextInputType.multiline,
+      textCapitalization: TextCapitalization.sentences,
+      scrollPadding: EdgeInsets.all(16.0),
+      onSaved: (value) {
+        setState(() {
+          _taskDescription = value;
+        });
+      },
+
+    ));
+
+
+    Future _selectDate() async {
+      DateTime picked = await showDatePicker(
+          context: context,
+          initialDate: _Date,
+          firstDate: new DateTime.now(),
+          lastDate: new DateTime(2022)
+      );
+      if(picked != null) setState(() => _Date = picked);
+    }
+
+    Future _selectTime() async {
+      TimeOfDay picked = await showTimePicker(
+          context: context,
+          initialTime: _Time
+      );
+      if (picked != null) setState(() => _Time = picked);
+    }
+
+    formWidget.add(new ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+          color: Colors.blueAccent[600],
+          child: new Icon(Icons.calendar_today),
+          onPressed: _selectDate,
+        ),
+        RaisedButton(
+          color: Colors.blueAccent[600],
+          child: new Icon(Icons.access_time),
+          onPressed: _selectTime,
+        ),],
+    ));
+
+    var formatter = new DateFormat("MMMMEEEEd");
+    formWidget.add(new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+          formatter.format(_Date) + " " + _Time.format(context)
+      )
+      ],
+    )
+    );
+
+    formWidget.add(new ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RaisedButton(
+          color: Colors.blue,
+          textTheme: ButtonTextTheme.primary,
+          child: new Icon(Icons.check),
+          onPressed: () {
+            if (formKey.currentState.validate()) {
+              var task = TaskInfo(_taskTitle, _taskDescription, _Date, _Time);
+              Navigator.of(context).pop();
+            }
+          }
+          )
+      ],
+    ));
+
+
+  return formWidget;
+  }
+}
+
+class taskForm extends StatefulWidget {
+  @override
+  _taskFormState createState() => new _taskFormState();
+}
+
+class TaskList extends StatefulWidget {
   @override
   taskListState createState() => taskListState();
 }
-
 
 class TaskInfo {
   String _title;
   String _description;
   bool finished;
+  DateTime Date;
+  TimeOfDay Time;
 
-  TaskInfo(String t, String descr){
+  TaskInfo(String t, String descr, DateTime D, TimeOfDay T) {
     this._title = t;
     this._description = descr;
     this.finished = false;
+    this.Date = D;
+    this.Time = T;
   }
 
-  String getTitle() {return this._title;}
-  String getDescription() {return this._description;}
+  String getTitle() {
+    return this._title;
+  }
+
+  String getDescription() {
+    return this._description;
+  }
 }
